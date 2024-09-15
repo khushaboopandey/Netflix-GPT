594171d6e42a2e17c152460875f36f4281c0fc7a
khushaboopandey <45198458+khushaboopandey@users.noreply.github.com>
2024-09-15 21:43:59 +0530

    VideoBackground component code changes

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -8,7 +8,18 @@ const VideoBackground = ({ moviesId }) => {
   useuseMovieTrailer(moviesId)
 
   return (
-    <div> <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + trailerVideo?.key} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
+    <div className='w-screen'>
+      <iframe
+        className='w-screen aspect-video'
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&cc_load_policy=1&mute=1`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen>
+
+      </iframe>
+    </div>
   )
 }
 
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className='p-36 p-12'>
+    <div className='w-screen aspect-video pt-[20%] p-12 absolute text-white bg-gradient-to-r from-black'>
       <h1 className='text-6xl font-bold'>{title}</h1>
       <p className='p-6 text-lg w-1/4'>{overview}</p>
       <div>
-        <button className='bg-gray-500 text-white p-4  px-12 text-xl bg-opacity-60 rounded  '>
+        <button className='bg-gray-500 text-white p-4  px-12 text-xl bg-opacity-60 rounded  hover:bg-opacity-50 '>
           ▶️ play
         </button>
         <button className='bg-gray-500 mx-2 text-white p-4  px-12 text-xl bg-opacity-60 rounded  '>
